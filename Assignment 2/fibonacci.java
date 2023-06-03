cdd10d01969a84aef9f6ca15d63cb348cbd86940
Nelson Wang <nelsonwangmain@outlook.com>
2023-06-02 21:21:10 -0400

    Implemented Question 2.

---
+++ Assignment 2/fibonacci.java
@@ -0,0 +1,90 @@
+import java.util.Scanner;
+
+//main thread of execution. 
+public class fibonacci {
+    public static void main(String args[]) {
+
+        int n = 0; //Number of fibonacci terms to calculate.
+        int[] result; //Fibonacci terms returned from thread. 
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter in the number of fibonacci numbers to generate:"); //Note that a '1' input denotes fibonacci term '0', so we want to get up to fibonacci term n-1.
+        
+        try {
+            n = scanner.nextInt();
+
+            while (n <= 0) {
+                System.out.println("Please input a number greater than 0.");
+                System.out.println("Enter in the number of fibonacci numbers to generate:");
+                n = scanner.nextInt();
+            }
+
+        } catch (Exception e) {
+    
+            System.out.println("Invalid input. Please input an integer.");
+        }
+
+        scanner.close();
+    
+        FibonacciThread calculateTerms = new FibonacciThread(n);
+        
+        calculateTerms.start(); //start the thread. 
+
+        try {
+            calculateTerms.join(); // Wait for the child thread.
+        } catch (Exception e) {
+            System.out.println("Failed to join thread.");
+        }
+
+        result = calculateTerms.getTerms(); //Get the fibonacci terms. 
+
+        //Print the fibonacci terms. 
+        System.out.println("Fibonacci terms: ");
+        for (int i = 0; i < result.length; i++){
+            System.out.print(result[i] + " ");
+
+        }
+        System.out.println();
+
+    } 
+    
+}
+
+//child thread. 
+class FibonacciThread extends Thread {
+    int n; 
+    int[] fibTerms; 
+
+    public FibonacciThread(int terms) {
+        n = terms;
+
+    }
+
+    public int[] getTerms() {
+        return fibTerms; 
+    }
+    
+    public void run() {
+        fibTerms = new int[n];
+
+        if (n == 1) {
+            fibTerms[0] = 0;
+        }
+
+        else { 
+            fibTerms[0] = 0;
+            fibTerms[1] = 1;
+
+            for (int i = 2; i < n; i++) {
+                fibTerms[i] = fibTerms[i - 1] + fibTerms[i - 2];
+            }
+        }
+    }
+
+
+}
+
+
+
+

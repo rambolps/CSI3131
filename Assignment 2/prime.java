9f1761501729ac7704ded04a09fb26b7f0ec15d3
rambolps <ryanrambali@gamil.com>
2023-06-03 17:37:02 -0400

    Finished part 1 and added a shell script to run

---
+++ Assignment 2/prime.java
@@ -0,0 +1,41 @@
+public class prime {
+    public static void main(String args[]){
+
+        //convert args into num and pass into thread
+        int numIn = Integer.valueOf(args[0]);
+        PrimeThread primeThread = new PrimeThread(numIn);
+
+        //start thread
+        primeThread.start();
+    }
+}
+
+class PrimeThread extends Thread{
+    int numOfTerms;
+
+    public PrimeThread(int numOfTerms){
+        //set number of terms to argument
+        this.numOfTerms = numOfTerms;
+    }
+
+    public void run(){
+        //go through all number less than or equal to user input
+        for(int i = 2; i <= numOfTerms; i++){
+
+            //assume prime, and test for divisibility
+            boolean isPrime = true;
+
+            for(int j = 2; j <= i/2; j++){
+                if(i % j == 0){
+                    isPrime = false;
+                    break;
+                }
+            }
+
+            //print if prime
+            if (isPrime) {
+                System.out.println(i);
+            }
+        }
+    }
+}
